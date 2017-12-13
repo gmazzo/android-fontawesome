80221709360673fe33934f11bda95c6c4ac049ba
guillermo.mazzola <guillermo.mazzola@globant.com>
2017-12-13 01:57:46 -0300

    Added sample project

---
+++ sample/src/main/java/gs/fontawesome/sample/MainAcitivty.java
@@ -0,0 +1,15 @@
+package gs.fontawesome.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAcitivty extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+    }
+
+}
